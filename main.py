08508f2467d4efa7de8b353119b2b6339f934952
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2023-11-21 00:04:52 -0500

    add neo name list

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ json_data = response.json() if response and response.status_code == 200 else Non
 formatted_json = json.dumps(json_data, indent=2) #format json response
 near_earth_objects = json_data["near_earth_objects"] #isoloate list of near earth objects
 i = 0
+neo_id_list = []    
+neo_name_list = []
+
 while i < len(near_earth_objects): #iterate through list of near earth ojects and gather names of each neo
     neo = near_earth_objects[i]
-    print(neo["name"])
+    #print(neo["id"])
+    neo_id_list.append(neo["id"])  #add id's to list variable
+    neo_name_list.append(neo["name"]) #add names to list variable
+
     i += 1
-    
-neo = near_earth_objects[0]
+
+
+
+print(neo_id_list)
+print(neo_name_list)
 file = open("data.txt", "w")
 file.write(formatted_json)
 file.close()
-#print(neo["name"])
