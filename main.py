4f019bedd3f82dd850cbd7ef2a8095fa25cafdea
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2023-11-20 23:50:21 -0500

    add loop to iterate through json data to gather names of NEOs

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import json
 import requests
 
-response = requests.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY')
+response = requests.get('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY')
 
 json_data = response.json() if response and response.status_code == 200 else None
-#print(json_data)
-print()
-print()
-print()
-close_objects = json_data["near_earth_objects"]
-print("Print each key-value pair from JSON response")
+formatted_json = json.dumps(json_data, indent=2) #format json response
+near_earth_objects = json_data["near_earth_objects"] #isoloate list of near earth objects
 i = 0
-for key, value in close_objects.items():
-    print(key, ":", value)
-    print()
-
-
-
-#file = open("data.txt", "a")
-#file.write(json_data)
-#file.close()
+while i < len(near_earth_objects): #iterate through list of near earth ojects and gather names of each neo
+    neo = near_earth_objects[i]
+    print(neo["name"])
+    i += 1
+    
+neo = near_earth_objects[0]
+file = open("data.txt", "w")
+file.write(formatted_json)
+file.close()
+#print(neo["name"])
